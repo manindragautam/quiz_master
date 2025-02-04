b65c25393933d7b331ab0648e18d7c9e50c0f242
Manindra Gautam <manindra.gautam@infobeans.com>
2025-02-04 21:46:25 +0530

    setup the database, created models

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,13 @@
-from app import create_app
+from app import create_app, db
 from flask import render_template
+from app.models import User, Subject, Chapter, Quiz, Question, Score
 
 app = create_app()
+
+@app.cli.command('db-create')
+def create_db():
+    db.create_all()
+    print("Database created!")
 
 @app.route("/")
 def home():
